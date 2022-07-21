8cc7d8eb6657e652dab5e24f7d2db2706b6c1a34
ISHITSUKA Hikaru <ishitsuka.hikaru@aist.go.jp>
2022-07-21 11:53:22 +0900

    add annotation data editing functions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-# import mycolorpy.colorlist as mcp
+import os
 import pandas as pd
 import PySimpleGUI as sg
 import time
@@ -18,6 +18,8 @@ FONTSTYLE = cv2.FONT_HERSHEY_SIMPLEX
 FONTSIZE = 1.2
 FRAME_WIDTH = 3840 // 2
 FRAME_HEIGHT = 2160 // 2
+ZOOM_WIDTH = FRAME_WIDTH // 3
+ZOOM_HEIGHT = FRAME_HEIGHT // 3
 
 
 def load_video_and_tracking():
@@ -62,13 +64,106 @@ def load_video_and_tracking():
     return Path(fp), Path(f_track)
 
 
-def read_tracks(f_txt):
+def edit_annotation(df, index):
+    layout_id = [
+        [
+            sg.Text('Tracking ID', size=(8, 1)),
+            sg.InputText(df.iloc[index, 1], size=(4, 1), key='new_id')
+        ],
+        [
+            sg.Checkbox('Edit this frame only', False, key='id_only_frame')
+        ]
+    ]
+    layout_bbox = [
+        [
+            sg.Text('Left', size=(6, 1)),
+            sg.InputText(df.iloc[index, 2], size=(5, 1), key='new_bbox_left')
+        ],
+        [
+            sg.Text('Top', size=(6, 1)),
+            sg.InputText(df.iloc[index, 3], size=(5, 1), key='new_bbox_top')
+        ],
+        [
+            sg.Text('Width', size=(6, 1)),
+            sg.InputText(df.iloc[index, 4], size=(5, 1), key='new_bbox_width')
+        ],
+        [
+            sg.Text('Height', size=(6, 1)),
+            sg.InputText(df.iloc[index, 5], size=(5, 1), key='new_bbox_height')
+        ]
+    ]
+    layout_class = [
+        [
+            sg.Text('Class ID', size=(10, 1)),
+            sg.InputText(df.iloc[index, 6], size=(4, 1), key='new_class_id')
+        ],
+        [
+            sg.Text('Sub Class ID', size=(10, 1)),
+            sg.InputText(df.iloc[index, 8], size=(4, 1), key='new_subclass_id')
+        ],
+        [
+            sg.Checkbox('Edit this frame only', False, key='class_only_frame')
+        ]
+    ]
+    layout_conf = [[sg.InputText(df.iloc[index, 7], size=(11, 1), key='new_conf')]]
+
+    fra_id = sg.Frame(layout=layout_id, title='Tracking', vertical_alignment='top')
+    fra_bbox = sg.Frame(layout=layout_bbox, title='BBox', vertical_alignment='top')
+    fra_class = sg.Frame(layout=layout_class, title='Class', vertical_alignment='top')
+    fra_conf = sg.Frame(layout=layout_conf, title='Confidence', vertical_alignment='top')
+    # fra_id = sg.Column(layout=layout_id, vertical_alignment='top')
+    # fra_bbox = sg.Column(layout=layout_bbox, vertical_alignment='top')
+    
+    layout = [
+        [fra_id, fra_bbox, fra_class, fra_conf],
+        [sg.Submit('submit'), sg.Cancel('cancel')]
+    ]
+
+    window = sg.Window(f'Editing', layout)
+    window.finalize()
+
+    while True:
+        event, values = window.read(timeout=None)
+        if event == 'cancel' or sg.WIN_CLOSED:
+            id = df.iloc[index, 1]
+            bbox_left = df.iloc[index, 2]
+            bbox_top = df.iloc[index, 3]
+            bbox_width = df.iloc[index, 4]
+            bbox_height = df.iloc[index, 5]
+            class_id = df.iloc[index, 6]
+            subclass_id = df.iloc[index, 8]
+            conf = df.iloc[index, 7]
+            break
+        elif event == 'submit':
+            print('\nEdit annotations:')
+            id          = values['new_id'];          print(df.iloc[index, 1], '=>', id)
+            bbox_left   = values['new_bbox_left'];   print(df.iloc[index, 2], '=>', bbox_left)
+            bbox_top    = values['new_bbox_top'];    print(df.iloc[index, 3], '=>', bbox_top)
+            bbox_width  = values['new_bbox_width'];  print(df.iloc[index, 4], '=>', bbox_width)
+            bbox_height = values['new_bbox_height']; print(df.iloc[index, 5], '=>', bbox_height)
+            class_id    = values['new_class_id'];    print(df.iloc[index, 6], '=>', class_id)
+            subclass_id = values['new_subclass_id']; print(df.iloc[index, 8], '=>', subclass_id)
+            conf        = values['new_conf'];        print(df.iloc[index, 7], '=>', conf)
+            break
+
+    window.close()
+
+    return TrackingData(id, bbox_left, bbox_top, bbox_width, bbox_height, class_id, subclass_id, conf)
+
+
+def read_tracks(f_txt, ftype='txt'):
+    # imp csv read
     names = [
         'frame_id', 'id', 'bbox_left', 'bbox_top', 'bbox_width',
         'bbox_height', 'class_id', 'conf'
     ]
     usecols = [0, 1, 2, 3, 4, 5, 10, 11]
-    return pd.read_csv(f_txt, sep=' ', names=names, usecols=usecols)
+    df = pd.read_csv(f_txt, sep=' ', names=names, usecols=usecols)
+
+    # add subclass_id
+    df['subclass_id'] = 0
+
+    return df
 
 
 def draw_annotations(img, df, cmap, values):
@@ -79,7 +174,7 @@ def draw_annotations(img, df, cmap, values):
         cls = CLASSES[clsid]
         id = int(row.id)
         conf = row.conf
-        org = int(row.bbox_left), int(row.bbox_top)-10
+        org = int(row.bbox_left), int(row.bbox_top) - 10
 
         if values['key_radio_detection']:
             color = [int(_*255) for _ in cmap(clsid % len(CLASSES))]
@@ -95,20 +190,24 @@ def draw_annotations(img, df, cmap, values):
             cv2.putText(img, txt, org, FONTSTYLE, FONTSIZE, color, thickness=2)
 
 
-def print_annotations(df, window, image, cmap, values):
-    if df.empty:
-        window['-table-'].update('')
+def update_annotations(df, window, image, cmap, values):
+    df_ = df.copy()
+    if df_.empty:
+        window['anno_table'].update('')
     else:
-        draw_annotations(image, df, cmap, values)
-        window['-table-'].update(df.to_numpy().tolist())
+        draw_annotations(image, df_, cmap, values)
+        df_['conf'] = df_['conf'].apply(lambda x: int(x*100))
+        window['anno_table'].update(df_.to_numpy().tolist())
+
+    return df_
 
         
-def layout_ids(ids):
+def layout_ids(ids, cols=29):
     ret = []
     for i, id in enumerate(ids):
-        if i % 10 == 0:
+        if i % cols == 0:
             tmp = []
-        elif i % 10 == 9:
+        elif i % cols == cols-1:
             ret.append(tmp)
         tmp.append(sg.Checkbox(f'{id:03d}', True, key=f'-{id}-'))
     ret.append(tmp)
@@ -116,15 +215,32 @@ def layout_ids(ids):
     return ret        
 
 
+class TrackingData:
+    def __init__(self, id, bbox_left, bbox_top, bbox_width, bbox_height, class_id, subclass_id, conf):
+        self.id = int(id)
+        self.bbox_left = int(bbox_left)
+        self.bbox_top = int(bbox_top)
+        self.bbox_width = int(bbox_width)
+        self.bbox_height = int(bbox_height)
+        self.class_id = int(class_id)
+        self.subclass_id = int(subclass_id)
+        self.conf = float(conf)
+
+        
 class TrackingDataEditingTools:
     def __init__(self):
-        self.fp, self.f_track = load_video_and_tracking()
-        # self.fp = Path('samples/A000.mp4')
-        # self.f_track = Path('samples/A000.txt')
+        # self.fp, self.file_txt = load_video_and_tracking()
+        self.fp = Path('samples/A000.mp4')
+        self.file_txt = Path('samples/A000.txt')
+        # self.fp = Path('samples/D005.mp4')
+        # self.file_txt = Path('samples/D005.txt')
         
-        if not len(self.fp.name) or not len(self.f_track.name):
+        if not self.fp.exists() or not self.file_txt.exists():
             sg.Popup('File not found.')
             exit()
+
+        # csv output
+        self.file_csv = Path(self.file_txt.stem + '.csv')
 
         # video capture
         self.cap = cv2.VideoCapture(str(self.fp))
@@ -146,12 +262,15 @@ class TrackingDataEditingTools:
             self.frame_end = self.num_frames - 1
 
             self.is_stop = True
+
+            self.zoom_left = self.width // 2
+            self.zoom_top = self.height // 2
         else:
             sg.Popup('Failed to load video.')
             exit()
 
         # tracking data
-        self.track = read_tracks(str(self.f_track))
+        self.track = read_tracks(str(self.file_txt))
         self.track_ids = set(self.track['id'])
         self.num_tracks = len(self.track_ids)
         self.df = self.track[self.track['frame_id'] == self.frame_id]
@@ -161,24 +280,18 @@ class TrackingDataEditingTools:
         self.cmap = plt.get_cmap(COLORMAP, len(CLASSES))
 
         # layout
-        layout_disp = [[sg.Image(filename='', key='-display-')]]
+        layout_zoom = [[sg.Image(filename='', key='sub_screen')]]
+        layout_file = [
+            [
+                sg.Text('Video', size=(7, 1)),
+                sg.InputText(os.path.abspath(str(self.fp)), size=(70, 1)),
+            ],
+            [
+                sg.Text('Tracking', size=(7, 1)),
+                sg.InputText(os.path.abspath(str(self.file_txt)), size=(70, 1)),
+            ]
+        ]
         layout_ctrl = [
-            [
-                sg.Text('Video File', size=(10, 1)),
-                sg.InputText(self.fp.name, size=(70, 1)),
-                # sg.FileBrowse(key='video_file'),
-            ],
-            [
-                sg.Text('Tracking File', size=(10, 1)),
-                sg.InputText(self.f_track.name, size=(70, 10)),
-                # sg.FileBrowse(key='tracking_file'),
-            ],
-            # [
-            #     sg.Button('Load')
-            # ],
-            [
-                sg.HorizontalSeparator()
-            ],
             [
                 sg.Button('|<'),
                 sg.Button('<'),
@@ -191,61 +304,70 @@ class TrackingDataEditingTools:
                     0,
                     1,
                     orientation='h',
-                    size=(35, 15),
+                    size=(33.5, 15),
                     key='-progress bar-',
                     enable_events=True
                 ),
                 sg.Text(self.frame_end),
-            ],
-            [
-                sg.HorizontalSeparator()
-            ],
-            [
-                sg.Text('Classes')
-            ],
+            ]
+        ]
+        layout_cls = [
             [
                 sg.Checkbox(v, True, key=f'-{v}-') for k, v in CLASSES.items()
-            ],
-            [
-                sg.HorizontalSeparator()
-            ],
-            [
-                sg.Text('BBox Coloring')
-            ],
+            ]
+        ]
+        layout_mode = [
             [
                 sg.Radio('detection', key='key_radio_detection', group_id='g_mode', default=True),
                 sg.Radio('tracking', key='key_radio_tracking', group_id='g_mode', default=False)
-            ],
-            [
-                sg.HorizontalSeparator()
-            ],
-            [
-                sg.Text('Tracking ID')
-            ],
-            *layout_ids(self.track_ids),
-            [
-                sg.HorizontalSeparator()
-            ],
-            [
-                sg.Table(
-                    self.df.to_numpy().tolist(),
-                    headings=self.df.columns.tolist(),
-                    auto_size_columns=False,
-                    vertical_scroll_only=False,
-                    num_rows=16,
-                    key='-table-'
-                )
-            ],
-            [
-                sg.Button('Quit')
             ]
         ]
+        layout_track = layout_ids(self.track_ids)
 
-        self.disp = sg.Frame(layout=layout_disp, title='', relief=sg.RELIEF_SUNKEN)
-        self.ctrl = sg.Frame(layout=layout_ctrl, title='', relief=sg.RELIEF_SUNKEN)
+        self.screen = sg.Image(filename='', key='screen')
+        self.anno_table = sg.Table(
+            self.df.to_numpy().tolist(),
+            headings=self.df.columns.tolist(),
+            def_col_width=9,
+            auto_size_columns=False,
+            hide_vertical_scroll=True,
+            num_rows=25,
+            key='anno_table',
+            enable_events=True
+        )
+
+        # Frames
+        self.fra_zoom = sg.Frame(layout=layout_zoom, title='', vertical_alignment='top')
+        self.fra_file = sg.Frame(layout=layout_file, title='', vertical_alignment='top')
+        self.fra_ctrl = sg.Frame(layout=layout_ctrl, title='', vertical_alignment='top')
+        self.fra_clss = sg.Frame(layout=layout_cls,  title='Classes', vertical_alignment='top')
+        self.fra_colr = sg.Frame(layout=layout_mode, title='BBox Coloring', vertical_alignment='top')
+        self.fra_trak = sg.Frame(layout=layout_track, title=f'Tracking IDs ({len(self.track_ids)})', vertical_alignment='top')
+
+        # Column
+        self.layout_col00 = sg.Column(
+            [
+                [self.screen],
+                [self.fra_trak]
+            ],
+            vertical_alignment='top'
+        )
+        self.layout_col10 = sg.Column(
+            [
+                [self.fra_zoom],
+                [self.fra_file],
+                [self.fra_ctrl],
+                [self.fra_clss],
+                [self.fra_colr],
+                [self.anno_table],
+                [sg.Button('Quit')]
+            ],
+            vertical_alignment='top'
+        )
 
     def run(self):
-        layout = [[self.disp, self.ctrl]]
+        layout = [[self.layout_col00, self.layout_col10]]
+
         window = sg.Window('Tracking Data Editing Tools', layout, location=(0, 0))
         window.finalize()
         self.event, values = window.read(timeout=0)
@@ -262,7 +384,12 @@ class TrackingDataEditingTools:
         try:
             while True:
                 self.event, values = window.read(timeout=0)
-                
+
+                # update annotations
+                # self.df = self.track[self.track['frame_id'] == self.frame_id]
+                # update_annotations(self.df, window, self.frame, self.cmap, values)
+
+                # events
                 if self.event == 'Quit':
                     break
 
@@ -305,9 +432,6 @@ class TrackingDataEditingTools:
                     self.cmap = plt.get_cmap(COLORMAP, NUM_COLOR)
 
                 window['-progress bar-'].update(self.frame_id)
-                # if self.is_stop and self.event == '__TIMEOUT__':
-                #     window['-progress bar-'].update(self.frame_id)
-                #     continue
 
                 # capture frame
                 self.ret, self.frame = self.cap.read()
@@ -317,16 +441,35 @@ class TrackingDataEditingTools:
                     # self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.frame_begin)
                     # self.num_frames = self.frame_begin
                     continue
-                
+
+                # update annotations
                 self.df = self.track[self.track['frame_id'] == self.frame_id]
-                print_annotations(self.df, window, self.frame, self.cmap, values)
+                update_annotations(self.df, window, self.frame, self.cmap, values)
+
+                if self.event == 'anno_table':
+                    # idx = values['anno_table'][0]
+                    td = edit_annotation(self.df, *values['anno_table'])
+                    update_annotations(self.df, window, self.frame, self.cmap, values)
+                    self.zoom_left = td.bbox_left - ZOOM_WIDTH // 2
+                    self.zoom_left = 0 if self.zoom_left < 0 else self.zoom_left
+                    self.zoom_top = td.bbox_top - ZOOM_HEIGHT //2
+                    self.zoom_top = 0 if self.zoom_top < 0 else self.zoom_top
+                    
                 
                 # update display
+                # main screen
                 _, data = cv2.imencode(
                     '.png',
                     cv2.resize(self.frame, dsize=(FRAME_WIDTH, FRAME_HEIGHT)),
                 )
-                window['-display-'].update(data=data.tobytes())
+                window['screen'].update(data=data.tobytes())
+                # sub screen
+                _, data1 = cv2.imencode(
+                    '.png',
+                    self.frame[self.zoom_top:self.zoom_top+ZOOM_HEIGHT,
+                               self.zoom_left:self.zoom_left+ZOOM_WIDTH]
+                )
+                window['sub_screen'].update(data=data1.tobytes())
 
                 if self.is_stop:
                     self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.frame_id)
